ef9e2634de7864dd0bb185efcdcd77f3cb3dd74a
Sumit Kumar Surana <sumit.srn@gmail.com>
2021-12-23 17:40:37 +0530

    Added jest addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,23 @@
+const path = require("path");
+
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "storybook-css-modules-preset"
+    "storybook-css-modules-preset",
+    "@storybook/addon-jest",
   ],
-  "framework": "@storybook/react"
-}
+  framework: "@storybook/react",
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@/components": path.resolve(__dirname, "../components/"),
+      "@/styles": path.resolve(__dirname, "../styles"),
+    };
+    return config;
+  },
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,14 @@
+import { addDecorator } from "@storybook/react"; // <- or your view layer
+import { withTests } from "@storybook/addon-jest";
+
+import results from "../.jest-test-results.json";
+
+addDecorator(
+  withTests({
+    results,
+  })
+);
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +17,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
