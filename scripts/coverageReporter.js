dbc4bde4fa04a8516351c02d6fca92da58d0b16a
Sumit Kumar Surana <sumit.surana@agrevolution.in>
2022-01-04 15:36:53 +0530

    Reduced the code for badge

--- scripts/coverageReporter.js
+++ scripts/coverageReporter.js
@@ -1,13 +1,16 @@
+const findColor = (value) =>
+  value < 80 ? "critical" : value < 90 ? "orange" : "success";
+
 try {
   const { total } = require(__dirname + "/../coverage/coverage-summary.json");
   const items = ["lines", "statements", "functions", "branches"];
   const listOut = [];
   items.forEach((key) => {
     const obj = total[key];
-    const out = `${key.toUpperCase()}=${obj.pct || 0}`;
-    listOut.push(out);
+    const out = `${key.toUpperCase()}::${obj.pct || 0}%`;
+    listOut.push(out, `${key.toUpperCase()}_COLOR::${findColor(obj.pct)}`);
   });
-    console.log(listOut.join('|'));
+  console.log(listOut.join("|"));
 } catch (e) {
   console.error(e);
 }
